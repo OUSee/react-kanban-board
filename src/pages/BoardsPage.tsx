35f378c4f872e82820ac3e9c91b9c6a12de6d155
OUSee <666rld999@gmail.com>
2024-10-11 13:48:13 +0300

    fixed some rewriten code and make it more readable

--- src/pages/BoardsPage.tsx
+++ src/pages/BoardsPage.tsx
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux";
 import { Board } from "../components/Board/Board";
 import { TaskType } from "../types";
 import { RootState } from "../redux/store";
+import { useMemo } from "react";
 
 export const BoardsPage = () => {
   const unresolved = useSelector((state: RootState) => state.tasks.unresolved);
@@ -9,33 +10,48 @@ export const BoardsPage = () => {
   const inProgress = useSelector((state: RootState) => state.tasks.inProgress);
   const resolved = useSelector((state: RootState) => state.tasks.resolved);
 
+  const boardsData = useMemo(() => {
+    return [
+      {
+        key: TaskType.unresolved,
+        type: TaskType.unresolved,
+        tasks: unresolved,
+        options: [],
+      },
+      {
+        key: TaskType.accepted,
+        type: TaskType.accepted,
+        tasks: accepted,
+        options: unresolved,
+      },
+      {
+        key: TaskType.inProgress,
+        type: TaskType.inProgress,
+        tasks: inProgress,
+        options: accepted,
+      },
+      {
+        key: TaskType.resolved,
+        type: TaskType.resolved,
+        tasks: resolved,
+        options: inProgress,
+      },
+    ];
+  }, [unresolved, accepted, inProgress, resolved]);
+
   return (
     <div className="page">
       <div className="boardsTable">
-        <Board
-          key={TaskType.unresolved}
-          type={TaskType.unresolved}
-          tasks={unresolved}
-          options={[]}
-        />
-        <Board
-          key={TaskType.accepted}
-          type={TaskType.accepted}
-          tasks={accepted}
-          options={unresolved}
-        />
-        <Board
-          key={TaskType.inProgress}
-          type={TaskType.inProgress}
-          tasks={inProgress}
-          options={accepted}
-        />
-        <Board
-          key={TaskType.resolved}
-          type={TaskType.resolved}
-          tasks={resolved}
-          options={inProgress}
-        />
+        {boardsData.map((element) => {
+          return (
+            <Board
+              key={element.key}
+              type={element.type}
+              tasks={element.tasks}
+              options={element.options}
+            />
+          );
+        })}
       </div>
     </div>
   );
--- src/pages/TaskPage.tsx
+++ src/pages/TaskPage.tsx
@@ -8,41 +8,37 @@ import { useEffect, useState } from "react";
 export const TaskPage = () => {
   const data = useParams();
   const taskID: string = data.taskId!;
-  const type: string = data.type!;
+  const taskType: string = data.type!;
   const accepted = useSelector((state: RootState) => state.tasks.accepted);
   const unresolved = useSelector((state: RootState) => state.tasks.unresolved);
   const inProgress = useSelector((state: RootState) => state.tasks.inProgress);
   const resolved = useSelector((state: RootState) => state.tasks.resolved);
   const [task, setTask] = useState<Task>();
 
-  const figureTask = () => {
-    switch (type) {
-      case TaskType.accepted: {
-        setTask(accepted.find((task: Task) => task.taskId === taskID));
-        break;
-      }
-      case TaskType.unresolved: {
-        setTask(unresolved.find((task: Task) => task.taskId === taskID));
-        break;
-      }
-      case TaskType.inProgress: {
-        setTask(inProgress.find((task: Task) => task.taskId === taskID));
-        break;
-      }
-      case TaskType.resolved: {
-        setTask(resolved.find((task: Task) => task.taskId === taskID));
-        break;
-      }
-      default: {
-        console.log("task not found");
-        break;
-      }
-    }
+  const figureTask = (arr: Task[]) => {
+    setTask(arr.find((task: Task) => task.taskId === taskID));
   };
 
   useEffect(() => {
-    figureTask();
+    switch (taskType) {
+      case TaskType.unresolved: {
+        figureTask(unresolved);
+        break;
+      }
+      case TaskType.accepted: {
+        figureTask(accepted);
+        break;
+      }
+      case TaskType.inProgress: {
+        figureTask(inProgress);
+        break;
+      }
+      case TaskType.resolved: {
+        figureTask(resolved);
+      }
+    }
   }, []);
+
   return (
     <div className="page">{task != undefined && <EditorCMP task={task} />}</div>
   );
